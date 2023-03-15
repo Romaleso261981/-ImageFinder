76fda8a3d88315461759b0796a5c77f9185c9f1e
Roman lesyo <ladiginscormag@gmail.com>
2023-03-15 13:56:21 +0200

    Update MovieDetailsPage.jsx

--- src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -24,25 +24,9 @@ function MovieDetailsPage() {
           <div className={styles.descMovie}>
             <h2>created: {data.created}</h2>
             <h2>name: {data.name}</h2>
-            {/* <p>Rating: {data.vote_average}</p> */}
-            {/* <h3>Overview</h3> */}
-            {/* <p>{data.overview}</p> */}
             <h3>Genres: {data.name}</h3>
-            {/* <ul>
-              {data.genres.map(data => {
-                return (
-                  <li key={data.id}>
-                    <p>{data.name}</p>
-                  </li>
-                );
-              })}
-            </ul> */}
           </div>
         </div>
-        {/* <div className={styles.reviews}>
-            <Link to={`/movies/${data.id}/cast`} state={state}>Cast</Link>
-            <Link to={`/movies/${data.id}/reviews`} state={state}>Reviews</Link>
-        </div> */}
         <Outlet />
       </>
     )
