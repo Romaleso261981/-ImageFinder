608d3d833b08baa7a56b069b74a87a638ed0cd94
Roman lesyo <ladiginscormag@gmail.com>
2023-03-15 12:38:27 +0200

    fix

---
+++ src/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import AuthForm from '../AuthForm/AuthForm';
+import Container from '../container/Container';
+import styles from './HomePage.module.css';
+
+function HomePage() {
+  return (
+    <Container>
+      <AuthForm />
+    </Container>
+  );
+}
+
+export default HomePage;
